1f22372a66baec20b56c4bfdbeb309dec86881f4
Parth.D <92713484+cryptpd@users.noreply.github.com>
2021-11-08 17:31:17 +0530

    Add files via upload
    via. Python

---
+++ File_Explorer.py
@@ -0,0 +1,46 @@
+
+from tkinter import *
+
+from tkinter import filedialog
+
+
+
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir="/",
+                                          title="Select a File",
+                                          filetypes=(("Text files",
+                                                      "*.txt*"),
+                                                     ("all files",
+                                                      "*.*")))
+
+    label_file_explorer.configure(text="File Opened: " + filename)
+
+
+window = Tk()
+
+window.title('File_Explorer')
+
+window.geometry("800x300")
+
+window.config(background="black")
+
+label_file_explorer = Label(window,
+                            text="File_Explorer",
+                            width=100, height=4,
+                            fg="red")
+
+button_explore = Button(window,
+                        text="Browse Files",
+                        command=browseFiles)
+
+button_exit = Button(window,
+                     text="Exit",
+                     command=exit)
+
+label_file_explorer.grid(column=1, row=1)
+
+button_explore.grid(column=1, row=2)
+
+button_exit.grid(column=1, row=3)
+
+window.mainloop()
